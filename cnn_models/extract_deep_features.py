948d15502c33d2d063daf523121b1349be13ff6d
Chianghaoyu <a0981610711@gmail.com>
2024-03-21 12:17:25 +0800

    Create extract_deep_features.py

---
+++ cnn_models/extract_deep_features.py
@@ -0,0 +1,162 @@
+
+import sys
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorrt as trt
+import tensorflow as tf
+import keras.backend as K
+from keras.models import Model, load_model
+
+from skimage.io import imread, imshow, concatenate_images
+from skimage.transform import resize
+from sklearn.utils import class_weight
+from keras.models import Sequential
+import nibabel as nib
+from skimage.transform import resize, rescale
+
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+
+import csv
+
+# import pickle
+
+# from joblib import dump
+survival_file_path = '/root/dataset/BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData/test_set.csv'#gtr_test_set.csv
+training_data_path = '/root/dataset/BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData'
+
+
+prediction_model_path = '/root/code/xai-for-brain-img-surv/models/regression_3d_scaled_v1/results/surv_pred_3d_scaled_v1'
+age_dict = {}
+days_dict = {}
+
+
+mri_type = 't1'
+
+
+prediction_model_path = prediction_model_path + "_" + mri_type
+
+
+
+
+def standardize(image):
+  standardized_image = np.zeros(image.shape)
+  # iterate over the `z` dimension
+  for z in range(image.shape[2]):
+    # get a slice of the image
+    # at channel c and z-th dimension `z`
+    image_slice = image[:, :, z]
+
+    # subtract the mean from image_slice
+    centered = image_slice - np.mean(image_slice)
+
+    # divide by the standard deviation (only if it is different from zero)
+    if (np.std(centered) != 0):
+      centered = centered / np.std(centered)
+
+    # update  the slice of standardized image
+    # with the scaled centered and scaled image
+    standardized_image[:, :, z] = centered
+
+  return standardized_image
+
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+# Read survival infos
+survival_data_count = 0
+max_survival_days = 0
+all_images = []
+with open(survival_file_path, mode='r') as csv_file:
+  csv_reader = csv.reader(csv_file, delimiter=',')
+  a = 0
+  b = 0
+  c = 0
+  first = True
+  for row in csv_reader:
+    if first == True:
+      print(f'Column names are {", ".join(row)}')
+      first = False
+    else:
+      print(row)
+      key = row[0]
+      age = row[1]
+      days = row[2]
+      age_dict[key] = float(age)
+      days_dict[key] = int(days)
+      max_survival_days = max(max_survival_days, int(days))
+      all_images.append(key)
+      if int(days) < 250:
+        a += 1
+      elif (int(days) >= 250 and int(days) <= 500):
+        b += 1
+      else:
+        c += 1
+      survival_data_count += 1
+
+  print(f'Processed {survival_data_count} survival data points.')
+  # age_m = np.zeros((1,1))
+  print(a, b, c)
+  print(max_survival_days)
+
+# Load prediction model
+surv_model = load_model(prediction_model_path + ".h5")
+layer_name = 'flatten'
+mid_layer_model = Model(inputs=surv_model.input, outputs=surv_model.get_layer(layer_name).output)
+
+epoch_loss = 0
+epoch_accu = 0
+input_to_model = np.zeros((1, 120, 120, 78), dtype=np.float16)
+age = np.zeros((1, 1))
+ground_truth = 0.0
+cnt = 0
+classification_count = 0.0
+sq_err = 0.0
+fig_x = []
+fig_acc = []
+
+for image_num in range(len(all_images)):
+  data = np.zeros((120, 120, 78))
+  x = all_images[image_num]
+
+  folder_path = training_data_path + '/' + x;
+  modalities = os.listdir(folder_path)
+  modalities.sort()
+
+  for j in range(len(modalities)):  # images get loaded here!
+
+    image_path = folder_path + '/' + modalities[j]
+    if (image_path.find(mri_type + '.nii') != -1):
+      img = nib.load(image_path);
+      image_data = img.get_fdata()
+      image_data = np.asarray(image_data)
+      image_data = rescale(image_data, 0.5, anti_aliasing=False)
+      # image_data = resize(image_data, (120, 120, 78))
+      image_data = standardize(image_data)
+      data = image_data
+
+      break
+
+  # print("Loading data point (" + str(cnt + 1) + "/" + str(len(all_images)) + "): " + x)
+  # print(image_data2.shape)
+
+  input_to_model[0] = data
+  age[0, 0] = float(age_dict[x])
+  days = int(days_dict[x])
+  ground_truth = int(days) / max_survival_days
+  cnt += 1
+
+  pred = mid_layer_model.predict(x=[input_to_model, age])
+  #print(np.asarray(pred).shape)
+  #print(pred)
+    
+
